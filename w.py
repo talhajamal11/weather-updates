4b811bc281905ba9c66140ca490e2814814e168f
Talha Jamal <talhajamal11@gmail.com>
2019-07-31 06:41:22 +0000

    Removed all comments

--- w.py
+++ w.py
@@ -10,20 +10,7 @@ web_hook_url = (
     "https://hooks.slack.com/services/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"
 )
 
-# SLACK_API_TOKEN = 'xoxb-7871756934-707612720407-NP0mKmHAPuUV7KVL4XAx8AZP'
 
-# client = slack.WebClient(token=os.environ['xoxb-7871756934-707612720407-NP0mKmHAPuUV7KVL4XAx8AZP'])
-
-# response = client.chat_postMessage(
-#       channel='#bykea-internship',
-#      text="Hello world!")
-# assert response["ok"]
-# assert response["message"]["text"] == "Hello world!"
-
-# slack = Slacker('xoxb-7871756934-707612720407-jejuMOFJbqlSpwyMYvmIl5aJ')
-
-
-# city = input("Enter your city: ")
 city = "Islamabad"
 url = "http://api.openweathermap.org/data/2.5/weather?q={},pk&APPID=d58366f88671fbbb22b4cc69734c45ba".format(
     city
@@ -37,16 +24,6 @@ humidity = data["main"]["humidity"]
 wind_speed = data["wind"]["speed"]
 description = data["weather"][0]["description"]
 
-
-# message = "'Temperature: ', temp \n"
-
-
-# print(res)
-
-# print('Temperature: ', temp)
-# print('Wind Speed: {}'.format(wind_speed))
-# print('Humidity: {}'.format(humidity))
-# print('Description: {}'.format(description))
 
 slack_city = {"text": city}
 slack_temp = {"text": "Temperature : {}".format(temp)}
